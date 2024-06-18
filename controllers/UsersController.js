40dc0597023ca1ed19fa7a2b60150f5020d8b5b7
ay0x <85720179+ay0x@users.noreply.github.com>
2024-06-18 15:22:00 +0200

    Users

---
+++ controllers/UsersController.js
@@ -0,0 +1,35 @@
+const crypto = require('crypto');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const usersCollection = dbClient.db.collection('users');
+    const existingUser = await usersCollection.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    const result = await usersCollection.insertOne(newUser);
+
+    return res.status(201).json({ id: result.insertedId, email });
+  }
+}
+
+module.exports = UsersController;
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const routes = require('./routes/index');
 
 const app = express();
 
+app.use(express.json());
+
 app.use('/', routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
