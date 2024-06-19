71976cc582fc5395306ccb251e65042067ee8d08
ay0x <85720179+ay0x@users.noreply.github.com>
2024-06-19 14:05:37 +0200

    Return to default port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use('/', routes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
