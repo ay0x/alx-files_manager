c79e5c6604e57d9d1a01bf4345cb7365d97a10c2
ay0x <85720179+ay0x@users.noreply.github.com>
2024-06-19 15:50:32 +0200

    Auth Controller and Users Controller

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,8 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
