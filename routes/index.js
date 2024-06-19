6baf7d2a37eb49a9655b69f971f4e311e043e298
ay0x <85720179+ay0x@users.noreply.github.com>
2024-06-19 13:53:32 +0200

    Changed import order

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
-const express = require('express');
-
-const router = express.Router();
+const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
